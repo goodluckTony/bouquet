780136629a3b361e9b0ba1845a0aeff0220b125c
Max Lobachov <mlobacov@gmail.com>
2022-10-13 18:01:55 +0300

    add alert to title bouq buttons and fix the scrolling issue

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const titleImgSeletion1 = document.querySelector(
+  ".title-right-selective-img-1"
+);
+titleImgSeletion1.addEventListener("click", () => {
+  alert("its slider, mazerfaker!");
+});
+
+const titleImgSeletion2 = document.querySelector(
+  ".title-right-selective-img-2"
+);
+titleImgSeletion2.addEventListener("click", () => {
+  alert("its slider, mazerfaker!");
+});
+
+const titleImgSeletion3 = document.querySelector(
+  ".title-right-selective-img-3"
+);
+titleImgSeletion3.addEventListener("click", () => {
+  alert("its slider, mazerfaker!");
+});
