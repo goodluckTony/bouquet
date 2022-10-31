3c782342f482895fca7d8e9b8e0739f5d9a30bf1
Max Lobachov <mlobacov@gmail.com>
2022-10-31 14:43:37 +0200

    some changes

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
-const titleImgSeletion1 = document.querySelector(
-  ".title-right-selective-img-1"
-);
+
+
+const titleImgSeletion1 = document.querySelector(".title-right-selective-img-1");
 titleImgSeletion1.addEventListener("click", () => {
-  alert("its slider, mazerfaker!");
+
+document.querySelector(".title-right-active-img").src = "../img/bouquet_4.png";
+document.querySelector(".title-rpc-active").classList.remove("title-rpc-active");
+document.querySelector("#circle1").classList.add("title-rpc-active");
 });
 
-const titleImgSeletion2 = document.querySelector(
-  ".title-right-selective-img-2"
-);
+const titleImgSeletion2 = document.querySelector(".title-right-selective-img-2");
 titleImgSeletion2.addEventListener("click", () => {
-  alert("its slider, mazerfaker!");
+
+document.querySelector(".title-right-active-img").src = "../img/bouquet_2.png";
+document.querySelector("title-rpc-active").classList.remove("title-rpc-active");
+document.querySelector("#circle2").classList.add("title-rpc-active");
 });
 
-const titleImgSeletion3 = document.querySelector(
-  ".title-right-selective-img-3"
-);
+const titleImgSeletion3 = document.querySelector(".title-right-selective-img-3");
 titleImgSeletion3.addEventListener("click", () => {
-  alert("its slider, mazerfaker!");
+
+document.querySelector(".title-right-active-img").src = "../img/bouquet_3.png";
+document.querySelector(".title-rpc-active").classList.remove("title-rpc-active");
+document.querySelector("#circle3").classList.add("title-rpc-active");
 });
